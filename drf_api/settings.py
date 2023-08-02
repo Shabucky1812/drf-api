5d41278fcc53f37c9d9c10f60bd327e0c6a6593c
Shabucky1812 <shaunbuck1812@gmail.com>
2023-08-02 09:07:35 +0000

    Adjust settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -112,10 +112,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    os.environ.get('CLIENT_ORIGIN'),
-    os.environ.get('CLIENT_ORIGIN_DEV'),
-]
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN'),
+        os.environ.get('CLIENT_ORIGIN_DEV')
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
